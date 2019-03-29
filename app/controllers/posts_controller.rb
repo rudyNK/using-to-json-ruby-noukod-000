e0b0fed96cb7b6a0b8d951e0cc70f8b30d340a47
Rudy Ravelin <rudy@noukod.com>
2019-03-29 13:29:55 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,9 +26,14 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  # def post_data
+  #   post = Post.find(params[:id])
+  #   render json: PostSerializer.serialize(post)
+  # end
+
   def post_data
     post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
+    render json: post.to_json
   end
 
 private
